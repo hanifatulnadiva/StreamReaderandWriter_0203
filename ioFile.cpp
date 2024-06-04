5dca01b12549d697e5967adeb0c8d1b0600c3b70
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-06-04 14:39:40 +0700

    menunjuk ke sebuah nama file

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,4 +8,7 @@ int main(){
     
     //membuka file dalam mode menulis
     ofstream outfile ;
+    //menunjuk ke sebuah nama file 
+    outfile.open("contohfile.txt");
+    cout << ">= menulis file,\'q\' untuk  keluar" << endl;
 }
