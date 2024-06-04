1c4ab292ac17460a6abe3f85515fbde0373d2511
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-06-04 14:44:33 +0700

    membuat perulangan selama tidak memasukkan karakter "q"

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,4 +11,15 @@ int main(){
     //menunjuk ke sebuah nama file 
     outfile.open("contohfile.txt");
     cout << ">= menulis file,\'q\' untuk  keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true){
+        cout<<"-";
+        //mendapatkan karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if(baris=="q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file 
+        outfile << baris <<endl;
+    }
 }
