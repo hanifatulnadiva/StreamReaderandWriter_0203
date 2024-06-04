c2454f24c9b9f2cca3738d75e4b7f47929e7792e
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-06-04 15:16:05 +0700

    menyelesaikan exception handling

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -7,10 +7,13 @@ int main()
 {
     cout <<"Awal Program" << endl; //penanda 1
     try{
-
+        array <int, 3> data = {3,5,7};
+        cout << data.at(5) << endl;
+        //memanggil elemen array
     }catch(exception& e)
     {
-
+        cout << e.what () << endl;
     }
-
+    cout << "baris program yang terakhir" << endl;
+    return 0;
 }
