5ecb27ebb480c9548974cb8be4060aeb7dae27fb
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-06-04 15:13:05 +0700

    menggunakan try catch untuk menangani kondisi eror pada saat program dijalankan

---
+++ exceptionStandard.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<exception>
+#include<array>
+using namespace std;
+
+int main()
+{
+    cout <<"Awal Program" << endl; //penanda 1
+    try{
+
+    }catch(exception& e)
+    {
+
+    }
+
+}
